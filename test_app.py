be5e289cff4ac5a5b0a2f562d3fc4b77257ee900
Shuhaib <114503177+Shuhaib73@users.noreply.github.com>
2025-06-01 12:42:57 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,11 +5,10 @@ from app import app
 
 @pytest.fixture
 def client():
-    # Setup the Flask test client
+    # Setup the Flask test client..
     with app.test_client() as client:
         yield client
 
 def test_home(client):
     response = client.get("/")
-
     assert response.status_code == 200
