7c7a74715c22a43486a012631c5ce20fa3bffb69
Shuhaib <114503177+Shuhaib73@users.noreply.github.com>
2025-08-02 18:53:00 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,18 +1,13 @@
 import os 
 import pytest 
 
-
 from app import app 
-
-
 
 @pytest.fixture
 def client():
     # Setup the Flask test client..
     with app.test_client() as client:
         yield client
-
-
 
 def test_home(client):
     response = client.get("/")
